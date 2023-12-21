7053499aeda763b7a1569fed66a6fd3c85c0ef63
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2023-12-21 18:32:40 +0530

    final commit

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -6,12 +6,9 @@ const useCurrencyInfo = (currency) => {
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
         .then((res) => res.json())
-        .then((res) => {
-            setData(res[data])
-        })
-
-        return data 
+        .then((res) => setData(res[currency]))
     }, [currency])
+    return data 
 }
 
 export default useCurrencyInfo
