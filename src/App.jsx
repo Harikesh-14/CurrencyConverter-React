304ba1b5ed2cdd8307d3362089f83f1b634e2ec3
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2023-12-21 11:58:44 +0530

    Did some major changes but there are some errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { useState } from 'react'
 import './App.css'
 import InputBox from './components/inputBox'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 function App() {
+    const [amount, setAmount] = useState(0)
+    const [from, setFrom] = useState("usd")
+    const [to, setTo] = useState("inr")
+    const [convertedAmount, setConvertedAmount] = useState(0)
+
+    const currencyInfo = useCurrencyInfo(from)
+
+    const options = Object.keys(currencyInfo)
+
+    const swap = () => {
+        setFrom(to)
+        setTo(from)
+        setConvertedAmount(amount)
+        setAmount(convertedAmount)
+    }
+
+    const conversion = () => {
+        setConvertedAmount(amount * currencyInfo[to])
+    }
 
     return (
         <>
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, {useId} from 'react'
 
-function InputBox() {
-  return (
-    <div>InputBox</div>
-  )
+function InputBox({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectCurrency = "usd",
+    amountDisable = false,
+    currencyDisable = false,
+    className = "",
+}) {
+    const amountInputId = useId()
+
+    return (
+        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+            <div className="w-1/2">
+                <label htmlFor={amountInputId} className='text-black/40 mb-2 inline-block'>{label}</label>
+                <input
+                    type="number"
+                    id={amountInputId}
+                    className='outline-none w-full bg-transparent py-1.5'
+                    placeholder='Amount'
+                    disabled={amountDisable}
+                    value={amount}
+                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+                />
+            </div>
+
+            <div className="w-1/2 flex flex-wrap justify-end text-right">
+                <p className="text-black/40 mb-2 w-full">Currency Type</p>
+                <select
+                    className='rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none'
+                    value={selectCurrency}
+                    onClick={(e) => onCurrencyChange && onCurrencyChange(Number(e.target.value))}
+                    disabled={currencyDisable}
+                >
+                    {
+                        currencyOptions.map((currency) => (
+                            <option key={currency} value={currency}>{currency}</option>
+                        ))
+                    }
+                </select>
+            </div>
+        </div>
+    )
 }
 
 export default InputBox
